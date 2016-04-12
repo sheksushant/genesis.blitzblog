254c2d7aeec85f390cd34897d9711c03fd9e4494
Sushant Shekhar <sushantshekhar20@gmail.com>
2016-04-12 12:02:00 +0530

    Added files via upload

---
+++ functions.php
@@ -0,0 +1,72 @@
+<?php
+/** Load directories */
+require_once( get_template_directory() . '/lib/init.php');
+
+/** Child theme (do not remove) */
+define( 'CHILD_THEME_NAME', __( 'BlitzBlog Theme', 'BlitzBlog' ) );
+define( 'CHILD_THEME_URL', 'http://www.futurebink.com/blitzblog' );
+define( 'CHILD_THEME_VERSION', '1.0.0' );
+
+//* Add HTML5 markup structure
+add_theme_support( 'html5' );
+
+//* Add Viewport meta tag for mobile browsers */
+add_theme_support( 'genesis-responsive-viewport' );
+
+//* Add support for custom background
+add_theme_support( 'custom-background' );
+
+/** Add support for 3-column footer widgets */
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+/** Support for various Image sizes */
+add_image_size('home', 220, 180, TRUE);
+add_image_size('home-featured', 580, 300, TRUE);
+
+//* Add post navigation (requires HTML5 theme support)
+add_action( 'genesis_entry_footer', 'genesis_prev_next_post_nav', 15 );
+
+/** Support for different color style options */
+add_theme_support( 'genesis-style-selector', array(
+	'BlitzBlog-blue'		=> 'Blue',
+	'BlitzBlog-green'	=> 'Green',
+	'BlitzBlog-black'	=> 'Black',
+	'BlitzBlog-white'		=> 'Hybrid',
+) );
+
+//* Load Lato and Merriweather Google fonts
+add_action( 'wp_enqueue_scripts', 'custom_load_google_fonts' );
+function custom_load_google_fonts() {
+	wp_enqueue_style( 'google-font', '//fonts.googleapis.com/css?family=Lato:300,400,700|PT+Sans:400,700', array(), PARENT_THEME_VERSION );
+}
+
+/** Reposition the breadcrumbs */
+remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+add_action( 'genesis_entry_header', 'genesis_do_breadcrumbs',7 );
+
+/** Customize breadcrumbs display */
+add_filter( 'genesis_breadcrumb_args', 'balance_breadcrumb_args' );
+function balance_breadcrumb_args( $args ) {
+	$args['home'] = 'Home';
+	$args['sep'] = ' ';
+	$args['list_sep'] = ', '; // Genesis 1.5 and later
+	$args['prefix'] = '<div class="breadcrumb"><div class="wrap">';
+	$args['suffix'] = '</div></div>';
+	$args['labels']['prefix'] = '<span class="home">You are here :</span>';
+	return $args;
+}
+
+add_filter( 'genesis_comment_list_args', 'childtheme_comment_list_args' );
+function childtheme_comment_list_args( $args ) {
+    $args['avatar_size'] = 40;
+	return $args;
+}
+
+/** Footer Copyright notice */
+add_filter('genesis_footer_output', 'footer_output_filter', 10, 3);
+function footer_output_filter( $output, $backtotop_text, $creds_text ) {
+    $creds_text = '<p>Copyright [footer_copyright] - <a href="'. trailingslashit( get_bloginfo('url') ) .'" title="'. esc_attr( get_bloginfo('name') ) .'" rel="dofollow">'.get_bloginfo('name').'</a>';
+
+	  $output = '<div class="gototop"> [footer_backtotop text="Return to Top" href="#"] </div>' . '<div class="creds">' . $creds_text . ' | Crafted by <a href="http://www.github.com/sushantshekhar20">@sushantshekhar20</a> from <a href="http://www.futureblink.com">FutureBlink</a> </p></div>' ;
+      return $output;
+}
